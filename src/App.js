1dedf1f24777218000c7b1fe27a1791b1cabfc83
Kefan Xie <xiekefan@gmail.com>
2022-09-29 22:03:51 -0400

    Converging the input component back to one component

--- src/App.js
+++ src/App.js
@@ -8,28 +8,11 @@ const currencies = [
   "BTC"
 ];
 
-class CurrencySelector extends React.Component {
-  constructor(){
-    super();
-  }
-
-  render() {
-    const items = currencies.map((currency) =>
-      <a href="#">{currency}</a>
-    );
-    return (
-      <div className="CurrencySelector">
-        {items}
-      </div>
-    );
-  }
-}
-
 function CurrencyInput(props) {
   return (
     <div className="CurrenyInput">
       <input type="text" />
-      <CurrencySelector />
+      <a className="CurrencySelector">{props.currency}</a>
     </div>
   );
 }
@@ -39,9 +22,9 @@ function ConversionDisplay(props) {
     <div className="ConversionDisplay">
       <h2>Currency Conversion</h2>
       <p>I want to spend</p>
-      <input type="text" />
+      <CurrencyInput currency="ETH" />
       <p>I want to buy</p>
-      <input type="text" />
+      <CurrencyInput currency="USD" />
     </div>
   );
 }
