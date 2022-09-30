64478015afc19716b6ef83eb7cf47ebff4fff1dc
Kefan Xie <xiekefan@gmail.com>
2022-09-29 22:27:28 -0400

    Pulling data from coinbase

--- src/App.js
+++ src/App.js
@@ -17,18 +17,46 @@ function CurrencyInput(props) {
   );
 }
 
-function ConversionDisplay(props) {
-  return (
-    <div className="ConversionDisplay">
-      <h2>Currency Conversion</h2>
-      <p>I want to spend</p>
-      <CurrencyInput currency="ETH" />
-      <p>I want to buy</p>
-      <CurrencyInput currency="USD" />
-      <p>Summary</p>
-      <p>You get XX USD for XX ETH</p>
-    </div>
-  );
+class ConversionDisplay extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      rates: {},
+      pair: ['ETH', 'USD']
+    }
+  }
+
+  pullData() {
+    fetch("https://api.coinbase.com/v2/exchange-rates?currency=USD")
+      .then((res) => res.json())
+      .then((json) => {
+        this.state.rates = json.data.rates
+      });
+  }
+
+  componentDidMount() {
+    this.pullData();
+    // this.loop = setInterval(() => this.pullData(), 30000);
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.loop);
+  }
+
+  render() {
+    return (
+      <div className="ConversionDisplay">
+        <h2>Currency Conversion</h2>
+        <p>I want to spend</p>
+        <CurrencyInput currency="ETH" />
+        <p>I want to buy</p>
+        <CurrencyInput currency="USD" />
+        <p>Summary</p>
+        <p>You get XX USD for XX ETH</p>
+      </div>
+    );
+  }
 }
 
 function App() {
