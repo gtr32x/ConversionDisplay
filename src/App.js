c49f798897d3b6e08b0ec32bcb64016a00cc910a
Kefan Xie <xiekefan@gmail.com>
2022-09-29 21:56:41 -0400

    General layout of the conversion display

--- src/App.js
+++ src/App.js
@@ -1,10 +1,47 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { Component } from "react"
+
+const currencies = [
+  "ETH",
+  "USD",
+  "BTC"
+];
+
+class CurrencySelector extends React.Component {
+  constructor(){
+    super();
+  }
+
+  render() {
+    const items = currencies.map((currency) =>
+      <a href="">{currency}</a>
+    );
+    return (
+      <div className="CurrencySelector">
+        {items}
+      </div>
+    );
+  }
+}
+
+function CurrencyInput(props) {
+  return (
+    <div className="CurrenyInput">
+      <input type="text" />
+      <CurrencySelector />
+    </div>
+  );
+}
 
 function ConversionDisplay(props) {
   return (
     <div className="ConversionDisplay">
-      <h1>Test</h1>
+      <h2>Currency Conversion</h2>
+      <p>I want to spend</p>
+      <input type="text" />
+      <p>I want to buy</p>
+      <input type="text" />
     </div>
   );
 }
