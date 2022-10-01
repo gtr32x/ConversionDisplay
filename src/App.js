c7f7a123cf7181dfb8c9f9f644d645ede5bfc28b
Kefan Xie <xiekefan@gmail.com>
2022-10-01 09:48:37 -0400

    Adding timer for each individual rates API call

--- src/App.js
+++ src/App.js
@@ -22,26 +22,52 @@ class ConversionDisplay extends React.Component {
     super(props);
 
     this.state = {
-      rates: {},
+      rates: {
+        'CB': {},
+        'CG': {}
+      },
+      rates_last_update_ts: {
+        'CB': 0,
+        'CG': 0
+      },
       rates_ready: false,
       pair: ['USD', 'ETH'],
       token: 'ETH',
       val1: 100,
       val2: 0,
       last_updated_val: 1,
-      last_updated_amt: 100
+      last_updated_amt: 100,
+      timer_is_set: false
     }
+  }
+
+  getCurrentTS() {
+    return Date.now() / 1000;
+  }
+
+  getRatesForToken(token) {
+    if (this.state.rates_ready){
+      if (this.state.rates_last_update_ts.CB >= this.getCurrentTS() - 40){
+        return this.state.rates.CB[token];
+      }
+
+      if (this.state.rates_last_update_ts.CG >= this.getCurrentTS() - 40){
+        return this.state.rates.CG[token];
+      }
+    }
+
+    return 0;
   }
 
   updateVal1(val) {
     this.setState({val1: val});
 
     if (this.state.rates_ready){
-      const token_amt = Math.round(val * this.state.rates[this.state.token] * 10000) / 10000;
+      const token_amt = Math.round(val * this.getRatesForToken(this.state.token) * 10000) / 10000;
       this.setState({val2: token_amt});
+      this.updateSummary();
     }
 
-    this.updateSummary();
     this.state.last_updated_amt = val;
     this.state.last_updated_val = 1;
   }
@@ -50,11 +76,11 @@ class ConversionDisplay extends React.Component {
     this.setState({val2: val});
 
     if (this.state.rates_ready){
-      const token_amt = Math.round(val / this.state.rates[this.state.token] * 10000) / 10000;
+      const token_amt = Math.round(val / this.getRatesForToken(this.state.token) * 10000) / 10000;
       this.setState({val1: token_amt});
+      this.updateSummary();
     }
 
-    this.updateSummary();
     this.state.last_updated_amt = val;
     this.state.last_updated_val = 2;
   }
@@ -64,28 +90,73 @@ class ConversionDisplay extends React.Component {
     this.setState({summary: str});
   }
 
-  pullData() {
+  pullDataCB() {
     fetch("https://api.coinbase.com/v2/exchange-rates?currency=USD")
       .then((res) => res.json())
       .then((json) => {
-        this.state.rates = json.data.rates;
+        this.state.rates.CB = json.data.rates;
         this.state.rates_ready = true;
+        this.state.rates_last_update_ts.CB = this.getCurrentTS();
 
         if (this.state.last_updated_val == 1){
           this.updateVal1(this.state.last_updated_amt);
         }else{
           this.updateVal2(this.state.last_updated_amt);
         }
+
+        console.log("CB updated");
+      });
+  }
+
+  pullDataCG() {
+    fetch("https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd")
+      .then((res) => res.json())
+      .then((json) => {
+        let rates = {};
+
+        for (const i of json){
+          rates[i.symbol.toUpperCase()] = 1/i.current_price;
+        }
+
+        this.state.rates.CG = rates;
+        this.state.rates_ready = true;
+        this.state.rates_last_update_ts.CG = this.getCurrentTS();
+
+        if (this.state.last_updated_val == 1){
+          this.updateVal1(this.state.last_updated_amt);
+        }else{
+          this.updateVal2(this.state.last_updated_amt);
+        }
+
+        console.log("CG updated");
       });
   }
 
   componentDidMount() {
-    this.pullData();
-    // this.loop = setInterval(() => this.pullData(), 30000);
+    // Upon component creation, call each rates API once to fetch the initial data
+    this.pullDataCG();
+    this.pullDataCB();
+
+    // Using a flag here to ensure that we will only ever set the timer once in the lifetime of this component to avoid unnecessary API calls that breach the rate limit
+    if (!this.state.timer_is_set){
+      this.loopCB = setInterval(() => this.pullDataCB(), 10000);
+      setTimeout(() => {
+        this.loopCG = setInterval(() => this.pullDataCG(), 10000);
+      }, 2000);
+      this.state.timer_is_set = true;
+    }
   }
 
   componentWillUnmount() {
-    clearInterval(this.loop);
+    if (this.loopCB){
+      clearInterval(this.loopCB);
+    }
+
+    if (this.loopCG){
+      clearInterval(this.loopCG);
+    }
+
+    this.state.timer_is_set = false;
   }
 
   render() {
